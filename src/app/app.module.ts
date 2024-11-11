f4aa8a2a939ecc8cdf45bf95ff9812a831ee5098
Alan Rios <alan.rios.hd@gmail.com>
2024-11-11 17:09:50 -0500

    Actualizar configuración de Firebase, implementar carga de imágenes de perfil y mejorar la navegación en la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,19 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"movil-2024b","appId":"1:1095177160052:web:6f2cb16ba99e8aec905821","storageBucket":"movil-2024b.firebasestorage.app","apiKey":"AIzaSyBYYBnkiZvkpKHY6gI9S7F0AhZltmprlto","authDomain":"movil-2024b.firebaseapp.com","messagingSenderId":"1095177160052","measurementId":"G-473NX1V5MN"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({
+    "projectId":environment.firebaseConfig.projectId,
+    "appId":environment.firebaseConfig.appId,
+    "storageBucket":environment.firebaseConfig.storageBucket,
+    "apiKey":environment.firebaseConfig.apiKey,
+    "authDomain":environment.firebaseConfig.authDomain,
+    "messagingSenderId":environment.firebaseConfig.messagingSenderId,
+    "measurementId":environment.firebaseConfig.measurementId})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  irebaseConfig:{
+  firebaseConfig:{
     apiKey: "AIzaSyBYYBnkiZvkpKHY6gI9S7F0AhZltmprlto",
     authDomain: "movil-2024b.firebaseapp.com",
     projectId: "movil-2024b",
