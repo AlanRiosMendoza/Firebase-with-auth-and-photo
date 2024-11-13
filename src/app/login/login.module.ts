a48d0320f859bf5011217246720ccfc634af0f26
cristian <paredes30@hotmail.es>
2024-11-13 10:39:06 -0500

    Login comit

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,10 +6,10 @@ import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
-import { LoginPage } from './LoginPage';
+import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, LoginPageRoutingModule, ReactiveFormsModule],
   declarations: [LoginPage]
 })
-export class LoginPageModule { }
+export class LoginPageModule {}
