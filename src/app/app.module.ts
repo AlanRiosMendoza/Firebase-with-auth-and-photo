7550097fb41299846f254fd6fe17f660746c56ab
Alan Rios <alan.rios.hd@gmail.com>
2024-11-13 22:34:27 -0500

    Agregar configuración inicial de Android, incluyendo archivos de proyecto, recursos y dependencias de Capacitor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,15 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({
+    "apiKey": "AIzaSyBYYBnkiZvkpKHY6gI9S7F0AhZltmprlto",
+    "authDomain": "movil-2024b.firebaseapp.com",
+    "projectId": "movil-2024b",
+    "storageBucket": "movil-2024b.firebasestorage.app",
+    "messagingSenderId": "1095177160052",
+    "appId": "1:1095177160052:web:6f2cb16ba99e8aec905821",
+    "measurementId": "G-473NX1V5MN"
+  })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
